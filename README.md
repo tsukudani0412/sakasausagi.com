c5194fca415ab9baaa3deb8f904e43decf25653f
Tsukudani <me@tkdn.jp>
2023-11-28 22:07:32 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# sakasausagi.com
+
+[Here!](https://sakasausagi.com)
+
+## Overview
+
+You can grab and rotate sakasausagi!
+
+## License
+
+MIT License
---
+++ script.js
@@ -0,0 +1,40 @@
+let deg = 0;
+let x = 0;
+let isDragging = false;
+
+const width = window.screen.width;
+const spanSakasa = document.getElementById('sakasa');
+const imgUsagi = document.getElementById('usagiimg');
+const guide = document.getElementById('guide');
+
+document.addEventListener('touchmove', e => {
+	e.preventDefault();
+});
+
+document.addEventListener('pointerdown', e => {
+	guide.style.display = "none";
+	x = e.pageX;
+	isDragging = true;
+});
+
+document.addEventListener('pointermove', e => {
+	if(isDragging === true) {
+		rotateImg(x, e.pageX);
+		x = e.pageX;
+	}
+});
+
+document.addEventListener('pointerup', e => {
+	isDragging = false;
+});
+
+function rotateImg(x, pagex) {
+	var deg_n = deg + (x-pagex) / 4;
+	deg = deg_n % 360;
+	imgUsagi.style.transform = "rotate(" + deg + "deg)";
+	if((deg <= -225) || ((-135 <= deg) && (deg <= 45)) || (225 <= deg)) {
+		spanSakasa.innerHTML = "sakasa ";
+	} else {
+		spanSakasa.innerHTML = "";
+	}
+}
